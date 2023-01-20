a35b71b756fc124a869a07db027f92ec4cf1ae7d
Trần Nhật Linh <linh54977@donga.edu.vn>
2023-01-20 22:57:03 +0700

    Thêm folder models

---
+++ My_App/lib/models/cateories_model.dart
@@ -0,0 +1,7 @@
+class CategorieModel {
+  late String categorieName;
+  late String imgUrl;
+
+
+  // Here, the variable "imgUrl" is defined as "late", which means that its value is not known at the time the object is created. The value of "myVariable" can be assigned at a later time, after the object has been created.
+}
